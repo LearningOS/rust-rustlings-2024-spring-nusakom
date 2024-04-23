55f2fe72e65a1d2e8dfb8c6d8b09e9c15e037134
TENGY_Z <110544069+nusakom@users.noreply.github.com>
2024-04-23 08:54:38 +0000

    Add new feature

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 struct Point {
     x: i32,
@@ -17,5 +17,5 @@ fn main() {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
-    y; // Fix without deleting this line.
+    let _ = y; // 使用 let _ 来抑制未使用变量的警告
 }
