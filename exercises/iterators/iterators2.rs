79778b4d70bdb3ff04c9cc907f1e336b23e8201b
TENGY_Z <110544069+nusakom@users.noreply.github.com>
2024-04-23 17:49:39 +0000

    Add new feature

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,33 +6,26 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-// Step 1.
-// Complete the `capitalize_first` function.
-// "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut result = String::new();
+            result.push(first.to_uppercase().next().unwrap());
+            result.push_str(c.as_str());
+            result
+        }
     }
 }
 
-// Step 2.
-// Apply the `capitalize_first` function to a slice of string slices.
-// Return a vector of strings.
-// ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
-// Step 3.
-// Apply the `capitalize_first` function again to a slice of string slices.
-// Return a single string.
-// ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 #[cfg(test)]
@@ -52,7 +45,7 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+        assert_eq!(capitalize_words_vector(&words), vec!["Hello", "World"]);
     }
 
     #[test]
